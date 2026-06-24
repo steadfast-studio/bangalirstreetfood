6911addc78123f77f7cf95fb08048103c086d70d
Sanchari Mandal <sancharim2233@gmail.com>
2026-06-25 01:27:03 +0530

    optimize wavy hero

--- components/WavyHero.tsx
+++ components/WavyHero.tsx
@@ -11,38 +11,35 @@ type WavyHeroProps = {
 
 const WavyHero = ({ title, subtitle, description, bgImage }: WavyHeroProps) => {
   return (
-    <div className="h-110 w-full overflow-x-hidden sm:h-150">
-      <div className="hero-mask-intersect relative h-full w-full">
-        {/* Background image */}
-        <Image
-          src={bgImage}
-          alt={title}
-          fill
-          priority
-          sizes="100vw"
-          className="object-cover object-center"
-        />
+    <div className="relative h-125 w-full overflow-hidden sm:h-150">
+      {/* Background image */}
+      <Image
+        src={bgImage}
+        alt={title}
+        width={1920}
+        height={1080}
+        priority
+        sizes="100vw"
+        fetchPriority="high"
+        className="h-full w-full object-cover object-center hero-mask-intersect"
+      />
 
-        {/* Overlay */}
-        <div className="absolute inset-0 bg-black/40" />
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/50 hero-mask-intersect" />
 
-        <div className="relative mx-auto flex h-80 w-full max-w-6xl flex-col items-start justify-end p-8 text-center text-white sm:h-100">
-          {subtitle && (
-            <p className="mb-2 text-xs font-semibold tracking-[0.25em] uppercase sm:text-sm">
-              {subtitle}
-            </p>
-          )}
+      {/* Content */}
+      <div className=" absolute inset-0 z-10 mx-auto flex max-w-6xl flex-col justify-end p-8 pb-52 text-white">
+        {subtitle && (
+          <p className="mb-2 text-xs font-semibold tracking-[0.25em] uppercase">
+            {subtitle}
+          </p>
+        )}
 
-          <h1 className="font-handwriting mb-4 text-4xl font-semibold sm:text-5xl md:text-6xl lg:text-7xl">
-            {title}
-          </h1>
+        <h1 className="mb-4 text-4xl font-semibold sm:text-6xl font-handwriting">{title}</h1>
 
-          {description && (
-            <p className="max-w-2xl text-left text-sm text-gray-100/90 sm:text-base">
-              {description}
-            </p>
-          )}
-        </div>
+        {description && (
+          <p className="max-w-2xl text-sm text-white/90">{description}</p>
+        )}
       </div>
     </div>
   );
